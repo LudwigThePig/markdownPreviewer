2a37dfd0a3144c2aeed9ff60dea2aa411d642d91
Morgan Galvin <morgangalvin@yahoo.com>
2018-12-17 13:18:49 +1300

    styled to fit in one view, adjusted markdown settings, added placeholder markdown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import marked from 'marked';
 
+marked.setOptions({
+  breaks: true
+})
+
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}` + '</a>';
+}
+
+
 class App extends Component {
   constructor(){
     super();
@@ -46,7 +56,7 @@ class Field extends Component {
   render(){
     return(
       <div id='editor'>
-        <textarea id='markdown-editor'rows="10" cols="50" value={this.props.markdown} onChange={this.props.handleChange}></textarea>
+        <textarea value={this.props.markdown} onChange={this.props.handleChange} spellcheck="false"></textarea>
       </div>
     )
   }
@@ -60,17 +70,37 @@ class Display extends Component{
     //   return { markup}
     // }
     return(
-      <div id='display' dangerouslySetInnerHTML={{__html: marked(this.props.markdown)}} />
+      <div id='preview' dangerouslySetInnerHTML={{__html: marked(this.props.markdown, )}} />
     )
   }
 }
 
 const placeholder = `
-<h1>TODO: Add a compelling placeholder</h1>
-  <ul>
-    <li>one</li>
-    <li>two</li>
-    <li>three</li>
-  </ul>`
+# TODO: Add a compelling placeholder
+
+[compelling idea](https://dictionary.cambridge.org/dictionary/english/compelling)
+
+## Your ideas can look like this:
+1. idea
+1. idea
+1. ikea
+1. idea
+
+### or like this...
+this is \`inline code\` 
+
+this is a code block
+
+\`\`\`javascript
+while(i !== 'idea'){
+  console.log('We need an idea!');
+}
+\`\`\`
+
+![ideaman](https://cnet2.cbsistatic.com/img/NYn58XCSVhK54AlJHwNmVgrfL6I=/570x0/2010/04/23/29f88fdd-fdb4-11e2-8c7c-d4ae52e62bcc/wozGRAY.jpg)
+now **THAT** is an idea man!
+  `
 
 export default App;
+
+//(H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
